3efc661dfa1d95134037efe53a11fbb2f38be669
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2024-05-17 08:30:55 +0000

    Deploying first page

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/home")
+def hello_world():
+    return "<p>Hellowww this is some  sabre alam, World!</p>"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080 , debug=True)
+
+ 
